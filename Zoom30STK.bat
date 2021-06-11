97ef1ba4dcbeb1a5b179691cbefc78a027160e38
Surya Tony Stark <59388153+SuryaTonyStark@users.noreply.github.com>
2021-06-12 01:59:13 +0530

    Update Zoom30STK.bat

--- Zoom30STK.bat
+++ Zoom30STK.bat
@@ -1,32 +1,31 @@
 ECHO OFF
-start "Zoom - Copy.exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy.exe"
-start "Zoom - Copy (2).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (2).exe"
-start "Zoom - Copy (3).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (3).exe"
-start "Zoom - Copy (4).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (4).exe"
-start "Zoom - Copy (5).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (5).exe"
-start "Zoom - Copy (6).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (6).exe"
-start "Zoom - Copy (7).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (7).exe"
-start "Zoom - Copy (8).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (8).exe"
-start "Zoom - Copy (9).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (9).exe"
-start "Zoom - Copy (10).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (10).exe"
-start "Zoom - Copy (11).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (11).exe"
-start "Zoom - Copy (12).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (12).exe"
-start "Zoom - Copy (13).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (13).exe"
-start "Zoom - Copy (14).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (14).exe"
-start "Zoom - Copy (15).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (15).exe"
-start "Zoom - Copy (16).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (16).exe"
-start "Zoom - Copy (17).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (17).exe"
-start "Zoom - Copy (18).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (18).exe"
-start "Zoom - Copy (19).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (19).exe"
-start "Zoom - Copy (20).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (20).exe"
-start "Zoom - Copy (21).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (21).exe"
-start "Zoom - Copy (22).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (22).exe"
-start "Zoom - Copy (23).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (23).exe"
-start "Zoom - Copy (24).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (24).exe"
-start "Zoom - Copy (25).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (25).exe"
-start "Zoom - Copy (26).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (26).exe"
-start "Zoom - Copy (27).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (27).exe"
-start "Zoom - Copy (28).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (28).exe"
-start "Zoom - Copy (29).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (29).exe"
-start "Zoom - Copy (30).exe" "C:\Users\SuryaTonyStark\AppData\Roaming\Zoom\bin\Zoom - Copy (30).exe"
-
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom1.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom2.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom3.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom4.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom5.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom6.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom7.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom8.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom9.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom10.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom11.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom12.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom13.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom14.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom15.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom16.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom17.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom18.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom19.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom20.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom21.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom22.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom23.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom24.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom25.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom26.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom27.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom28.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom29.exe
+start C:\Users\%USERNAME%\AppData\Roaming\Zoom\bin\Zoom30.exe
